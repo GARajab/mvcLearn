002cdf15aae4b5ad94cb0ff703efe9c3c54ebd26
GARajab <mohamed.rajab@ewa.com>
2024-11-09 20:55:06 +0300

    should be done 100%

--- server.js
+++ server.js
@@ -1,51 +1,77 @@
-const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config();
-const app = express();
-const mongoose = require("mongoose");
-const port = process.env.PORT;
-
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+const app = express()
+const mongoose = require("mongoose")
+const MvcRoutesExport = require("./models/MvcRoutes")
+const port = process.env.PORT
+const methodOverride = require("method-override")
+const morgan = require("morgan")
 // GET /
 app.get("/", async (req, res) => {
-  res.render("mvcEjs/index.ejs");
-});
+  const allMvc = await MvcRoutesExport.find()
+  res.render("mvcEjs/index", { mvcEjs: allMvc })
+})
 
 // connect to db
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI)
 // log if connected to db
 mongoose.connection.on("connected", () => {
   console.log(
     `You are connected to db (${mongoose.connection.name}) successfully`
-  );
-});
+  )
+})
 
-const MvcRoutesExport = require("./models/MvcRoutes");
+
 
 // get form of MVC
 app.get("/mvcEjs/new", (req, res) => {
-  res.render("mvcEjs/new.ejs");
-});
+  res.render("mvcEjs/new.ejs")
+})
+app.use(methodOverride("_method")) // new
+app.get("/:mvcEjsId", async (req, res) => {
+  const foundMvcEjs = await MvcRoutesExport.findById(req.params.mvcEjsId)
+  res.render("mvcEjs/show", { mvcEjs: foundMvcEjs })
+})
 // Set EJS as the view engine
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
-
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: false }))
+app.get("/mvcEjs/:mvcEjsId/edit", async (req, res) => {
+  const foundMvcEjs = await MvcRoutesExport.findById(req.params.mvcEjsId)
+  res.render("mvcEjs/edit.ejs", { mvc: foundMvcEjs })
+})
 // post form of MVC
 app.post("/mvcEjs", async (req, res) => {
   if (req.body.isMvcRoute === "on") {
-    req.body.isMvcRoute = true;
+    req.body.isMvcRoute = true
   } else {
-    req.body.isMvcRoute = false;
+    req.body.isMvcRoute = false
   }
-  await MvcRoutesExport.create(req.body);
-  res.render("mvcEjs/new");
-});
+  await MvcRoutesExport.create(req.body)
+  res.redirect("/")
+})
 
 app.get("/mvcEjs", async (req, res) => {
-  const allMvc = await MvcRoutesExport.find();
-  res.render("mvcEjs/index.ejs", { mvcEjs: allMvc });
-});
+  const allMvc = await MvcRoutesExport.find()
+  res.render("mvcEjs/index", { mvcEjs: allMvc })
+})
 
+app.delete("/mvcEjs/:mvcEjsId", async (req, res) => {
+  await MvcRoutesExport.findByIdAndDelete(req.params.mvcEjsId)
+  res.redirect("/")
+})
+
+app.put("/mvcEjs/:mvcEjsId", async (req,res)=>{
+  if(req.body.isMvcRoute === "on"){
+    req.body.isMvcRoute = true
+  }else{
+    req.body.isMvcRoute = false
+  }
+  await MvcRoutesExport.findByIdAndUpdate(req.params.mvcEjsId, req.body)
+  res.redirect(`/${req.params.mvcEjsId}`)
+})
+app.use(morgan("dev"))
 // run the server
 app.listen(port, () => {
-  console.log(`localhost:${port}`);
-});
+  console.log(`localhost:${port}`)
+})
